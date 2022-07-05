e6d6bdab6770e85627139eb53db47ea16cc8297b
mngom <ngomam1998@gmail.com>
2022-07-05 19:22:55 +0200

    Added the player_view to the player and added the controls on xml.

--- app/src/main/java/com/muse/exoplayer_playground/MainActivity.kt
+++ app/src/main/java/com/muse/exoplayer_playground/MainActivity.kt
@@ -6,59 +6,55 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.MediaMetadata
-import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.ui.PlayerControlView
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.util.Util
 
 class MainActivity : ComponentActivity(), Player.Listener {
 
     private lateinit var player: ExoPlayer
-    private var styledPlayerControlView: PlayerControlView ?= null
-    private var playerView: PlayerControlView ?= null
-    private var progressBar: ProgressBar ?= null
-    private var titleTv: TextView ?= null
+    private var playerView: PlayerView ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+
         setupPlayer()
         addMp3Files()
-//        setContent {
-//
-//        }
     }
 
     private fun setupPlayer() {
         player = ExoPlayer.Builder(this).build()
-        styledPlayerControlView?.player = player
+        playerView = findViewById(R.id.video_view)
+        playerView?.player = player
         player.addListener(this)
-
     }
 
+
     private fun addMp3Files() {
-        val mediaItem = MediaItem.fromUri("https://storage.googleapis.com/exoplayer-test-media-0/play.mp3")
+        //"https://storage.googleapis.com/exoplayer-test-media-0/play.mp3"
+        val mediaItem = MediaItem.fromUri("https://storage.googleapis.com/exoplayer-test-media-0/BigBuckBunny_320x180.mp4")
         player.addMediaItem(mediaItem)
         player.prepare()
         player.play()
     }
 
-    override fun onPlaybackStateChanged(playbackState: Int) {
-        super.onPlaybackStateChanged(playbackState)
-
-        when(playbackState){
-            Player.STATE_BUFFERING -> {
-                progressBar?.visibility = View.VISIBLE
-            }
-            Player.STATE_READY -> {
-                progressBar?.visibility = View.INVISIBLE
-            }
-        }
+    public override fun onStart() {
+        super.onStart()
     }
 
-    override fun onMediaMetadataChanged(mediaMetadata: MediaMetadata) {
-        super.onMediaMetadataChanged(mediaMetadata)
+//    override fun onPlaybackStateChanged(playbackState: Int) {
+//        super.onPlaybackStateChanged(playbackState)
+//
+//        when(playbackState){
+//            Player.STATE_BUFFERING -> {
+//                progressBar?.visibility = View.VISIBLE
+//            }
+//            Player.STATE_READY -> {
+//                progressBar?.visibility = View.INVISIBLE
+//            }
+//        }
+//    }
 
-        titleTv?.text = mediaMetadata.title ?: mediaMetadata.displayTitle ?: "title not found"
-    }
 }
